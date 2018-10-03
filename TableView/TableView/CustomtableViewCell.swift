b3688e1e1c54c4fe1da2b0c4bb4dff4c9f44d096
Deeksha Kaul <de32222t@L-156171311.local>
2018-10-03 14:52:31 +0530

    table view example

---
+++ TableView/TableView/CustomtableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomtableViewCell.swift
+//  TableView
+//
+//  Created by Deeksha Kaul on 03/10/18.
+//  Copyright © 2018 Deeksha Kaul. All rights reserved.
+//
+
+import UIKit
+
+class CustomtableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Deeksha Kaul on 03/10/18.
+//  Copyright © 2018 Deeksha Kaul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var data: [String] = []
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        for i in 0...100 {
+            data.append("\(i)")
+        }
+
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int{
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! CustomtableViewCell
+        if indexPath.row % 2 == 0{
+            cell.contentView.backgroundColor = UIColor.red
+        }
+        else{
+            cell.contentView.backgroundColor = UIColor.gray
+        }
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let alertController = UIAlertController(title: "Hint", message: "You have seleted roe \(indexPath.row)", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+}
+
